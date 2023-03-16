4d1dffbfb9a9f0a6a7f5f5ed3e2a94e5180e4e83
vartika2105 <127919694+vartika2105@users.noreply.github.com>
2023-03-16 02:06:12 -0700

    Update crash course array loop.c

--- crash course array loop.c
+++ crash course array loop.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int array[5];
+    int average = 1;
+    printf("Enter array\n");
+    for(int i=0;i<5;i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    for(int j=0;j<5;j++)
+    {
+        average+=array[j];
+    }
+    printf("average of elements=%d",average/5);
+    printf( "%d",array[2]);
+    
+}
